b11058c4b87bf24aa43e4625c068d97ba84a48d7
701i <anxelilk@gmail.com>
2022-08-26 11:49:54 -0500

    solved numerical series

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
+// import './App.css';
+import './calculations'
+import Form from './components/form'
 import formula from './formula.png';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Calculadora de series</h1>
+      </header>
+      <article>
         <img src={formula} className="formula" alt="formula" />
         <p>
           Introduce un numero y tu resultado sera acorde a esta formula
         </p>
-      </header>
+        <section>
+          <Form/>
+        </section>
+      </article>
     </div>
   );
 }
